af1472308af8a22d73108c7191711147b10cf9e8
smengo <s8901726x@majorana-giorgi.edu.it>
2024-11-25 09:41:26 +0100

    Update start.java

--- src/test_repository/start.java
+++ src/test_repository/start.java
@@ -2,8 +2,17 @@ package test_repository;
 
 public class start {
 	public static void main(String[] args) {
+		private int a;
 		System.out.println("ciao");
 		System.out.println("ciao2");
 		System.out.println("modifica github online");
+		a=12;
+		if(a<=10){
+			System.out.println("no");
+		}else
+		{
+			System.out.println("si");
+		}
+		
 	}
 }
